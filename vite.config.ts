54ce8a70129ed98d57bd4a8228f1b834e762ceb4
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2026-01-15 00:58:07 +0100

    Update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import { VitePWA } from "vite-plugin-pwa";
+import { type ManifestOptions, VitePWA } from "vite-plugin-pwa";
+
+type MissingManifestTypes = {
+  version: number;
+  manifest_version: number;
+  permissions?: string[];
+};
+
+const manifest: MissingManifestTypes & Partial<ManifestOptions> = {
+  version: 1,
+  manifest_version: 3,
+  name: "Squash Pay",
+  short_name: "Squash Pay",
+  description: "Split squash payments and share expenses with friends",
+  display: "standalone",
+  orientation: "portrait-primary",
+  icons: [
+    {
+      src: "/icon-192.png",
+      sizes: "192x192",
+      type: "image/png",
+      purpose: "any",
+    },
+    {
+      src: "/icon-512.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "any",
+    },
+    {
+      src: "/icon-1024.png",
+      sizes: "1024x1024",
+      type: "image/png",
+      purpose: "any",
+    },
+  ],
+  categories: ["finance", "utilities"],
+  permissions: ["clipboardWrite"],
+};
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,34 +49,7 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["**/*"],
-      manifest: {
-        name: "Squash Pay",
-        short_name: "Squash Pay",
-        description: "Split squash payments and share expenses with friends",
-        display: "standalone",
-        orientation: "portrait-primary",
-        icons: [
-          {
-            src: "/icon-192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/icon-1024.png",
-            sizes: "1024x1024",
-            type: "image/png",
-            purpose: "any",
-          },
-        ],
-        categories: ["finance", "utilities"],
-      },
+      manifest: manifest,
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2}"],
       },
