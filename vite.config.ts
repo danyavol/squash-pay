70702d160b11b8700e70a393c36e6ffe67c83c2e
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2026-02-08 16:55:11 +0100

    feat: Update icons and SW

--- vite.config.ts
+++ vite.config.ts
@@ -4,37 +4,43 @@ import svgr from "vite-plugin-svgr";
 import { type ManifestOptions, VitePWA } from "vite-plugin-pwa";
 
 type MissingManifestTypes = {
-  version: number;
+  version: string;
   manifest_version: number;
   permissions?: string[];
 };
 
 const manifest: MissingManifestTypes & Partial<ManifestOptions> = {
-  version: 1,
   manifest_version: 3,
   name: "Squash Pay",
+  version: "1.0.0",
   short_name: "Squash Pay",
   description: "Split squash payments and share expenses with friends",
   display: "standalone",
   orientation: "portrait-primary",
   icons: [
     {
-      src: "/icon-192.png",
+      src: "/pwa-192x192.png",
       sizes: "192x192",
       type: "image/png",
       purpose: "any",
     },
     {
-      src: "/icon-512.png",
+      src: "/pwa-512x512.png",
       sizes: "512x512",
       type: "image/png",
       purpose: "any",
     },
     {
-      src: "/icon-1024.png",
-      sizes: "1024x1024",
+      src: "/pwa-maskable-192x192.png",
+      sizes: "192x192",
       type: "image/png",
-      purpose: "any",
+      purpose: "maskable",
+    },
+    {
+      src: "/pwa-maskable-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "maskable",
     },
   ],
   categories: ["finance", "utilities"],
@@ -48,10 +54,11 @@ export default defineConfig({
     svgr(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["**/*"],
+      // Manifest icons are added using 'workbox.globPatterns'
+      includeManifestIcons: false,
       manifest: manifest,
       workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2}"],
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
     }),
   ],
